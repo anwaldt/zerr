8fed29253f9799e75c5c86f9dd990ef02d0c8c2d
hvc <contact@hvc.berlin>
2023-01-26 20:19:33 +0100

    Added line layout!

---
+++ start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+CPP/WeightShifter/weightshifter -z config/zerr.yaml &
+/media/anwaldt/ANWALDT_DATA/SOUND/PROJECTS/2022/Zerraeumlichung/config/line.yaml
+/media/anwaldt/ANWALDT_DATA/SOUND/PROJECTS/2022/Zerraeumlichung/
